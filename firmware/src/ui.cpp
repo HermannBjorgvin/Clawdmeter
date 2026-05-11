68a31b189b7a413787e7f938f41e84f54de86d26
Hermann Björgvin Haraldsson <hermann@hermann.is>
2026-05-11 00:16:56 +0000

    Add @amaanbuilds animation attribution to Bluetooth screen

--- firmware/src/ui.cpp
+++ firmware/src/ui.cpp
@@ -545,7 +545,13 @@ static void init_bluetooth_screen(lv_obj_t* scr) {
     lv_label_set_text(lbl_credit, "Built by @hermannbjorgvin");
     lv_obj_set_style_text_font(lbl_credit, &font_styrene_24, 0);
     lv_obj_set_style_text_color(lbl_credit, COL_DIM, 0);
-    lv_obj_align(lbl_credit, LV_ALIGN_BOTTOM_MID, 0, -24);
+    lv_obj_align(lbl_credit, LV_ALIGN_BOTTOM_MID, 0, -46);
+
+    lv_obj_t* lbl_credit2 = lv_label_create(ble_container);
+    lv_label_set_text(lbl_credit2, "Clawd animations by @amaanbuilds");
+    lv_obj_set_style_text_font(lbl_credit2, &font_styrene_20, 0);
+    lv_obj_set_style_text_color(lbl_credit2, COL_DIM, 0);
+    lv_obj_align(lbl_credit2, LV_ALIGN_BOTTOM_MID, 0, -20);
 
     // Start hidden
     lv_obj_add_flag(ble_container, LV_OBJ_FLAG_HIDDEN);
