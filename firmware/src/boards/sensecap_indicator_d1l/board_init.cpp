5269c5e82abec2fdb36565cf6fd83a5b09a644de
Christophe Vanlancker <carroarmato0@gmail.com>
2026-05-29 13:17:08 +0200

    Wire io_expander_init into board_init for SenseCap

--- firmware/src/boards/sensecap_indicator_d1l/board_init.cpp
+++ firmware/src/boards/sensecap_indicator_d1l/board_init.cpp
@@ -1,7 +1,9 @@
 #include "board.h"
+#include "io_expander.h"
 #include <Arduino.h>
 #include <Wire.h>
 
 extern "C" void board_init(void) {
     Wire.begin(IIC_SDA, IIC_SCL);
+    io_expander_init();
 }
